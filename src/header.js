cb6c682caaa4f1a2c527f03a808d6c26c2e31bfd
lucaswatts <75272509+lucaswatts@users.noreply.github.com>
2021-05-30 00:55:20 -0600

    123

---
+++ src/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function header() {
+    return (
+        <div>
+        <h1>Flash</h1>
+        <p>Flash memory cards</p>
+        </div>
+    )
+}
+
+export default header;
+
