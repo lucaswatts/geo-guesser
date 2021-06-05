ba73627dc52c385cf9ee90146e77eaf44764cb75
Andrew Bennett <artosis@Andrews-MacBook-Pro.local>
2021-06-04 21:10:11 -0400

    Fix Header file

--- src/header.js
+++ src/header.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import Button from './Button';
+import React from "react";
+import Button from "./Button";
 
 function Header() {
-    const onClick = () => {
-        console.log('hello');
-    }
-    return (
-        <div>
-            {/* <h1>Capital</h1> */}
-            {/* <Button color='green' text='Start' onClick={onClick} /> */}
-            <p id="display-timer">10:00</p>
-        </div>
-    )
+  const onClick = () => {
+    console.log("hello");
+  };
+  return (
+    <div>
+      {/* <h1>Capital</h1> */}
+      {/* <Button color='green' text='Start' onClick={onClick} /> */}
+      <p id="display-timer">10:00</p>
+    </div>
+  );
 }
 
 export default Header;
-
