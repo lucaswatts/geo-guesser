1a57e5146ea8304442139d878c64b02d82e11afc
lucaswatts <75272509+lucaswatts@users.noreply.github.com>
2021-06-13 17:15:00 -0600

    heavy UI changes and commented out unneccesary lines in country.js

--- src/components/Country.js
+++ src/components/Country.js
@@ -13,26 +13,25 @@ class Country extends React.Component {
       countries: [],
       currentIdx: 0,
       correctGuesses: [],
-      // progress: 0,
     };
   }
 
-addGuessedFieldToCountryData = (dataArray) => {
-  return dataArray.map((arrayItems) => {
-    return {
-      city: arrayItems.city,
-      country: arrayItems.country,
-      guessed: false,
-    }
-  });
-}
+// addGuessedFieldToCountryData = (dataArray) => {
+//   return dataArray.map((arrayItems) => {
+//     return {
+//       city: arrayItems.city,
+//       country: arrayItems.country,
+//       guessed: false,
+//     }
+//   });
+// }
 
   startGame = () => {
     const shuffledCountries = shuffle(countries);
-    const gameReadyCountries = this.addGuessedFieldToCountryData(shuffledCountries);
+    // const gameReadyCountries = this.addGuessedFieldToCountryData(shuffledCountries);
 
     this.setState({
-      countries: gameReadyCountries,
+      countries: shuffledCountries,
       gameStarted: true,
     });
   };
@@ -50,15 +49,6 @@ addGuessedFieldToCountryData = (dataArray) => {
     });
   }
 
-  updateRemainingPlaces() {}
-
-  updateScore() {}
-
-  //we need to clear the input box
-  //increment the array position
-  //we need to generate a new country
-  //we want to increment the users score if correct
-  //store whether or not the answer was correct 
   check = (event) => {
     console.log(this.state.countries[this.state.currentIdx].country);
     const input = event.target.value.toLowerCase();
@@ -94,14 +84,8 @@ addGuessedFieldToCountryData = (dataArray) => {
     })
   }
 
-
-  progressBar() {}
-//add if index is at the end, display the game over page
   render() {
 
-  //if game is over, show game over page
-  //if game is not over and the game has started, show the game content 
-  //if the game has not started and the game is not over, show the pre game content
     const gameOver = this.state.gameOver;
     const gameStarted = this.state.gameStarted;
     let page;
@@ -144,7 +128,7 @@ addGuessedFieldToCountryData = (dataArray) => {
             <button onClick={this.endGame} id="endGameBtn" className="btn">
               End Game
             </button>
-            
+      
           </div>
       )
     } 
@@ -162,6 +146,7 @@ addGuessedFieldToCountryData = (dataArray) => {
             >
               Start
             </button>
+            <p id="startMsg">Geo quiz will show you a random City from one of the world's Countries. Try and guess which Country the City belongs to. Good luck!</p>
           </div>
       )
     }
@@ -169,44 +154,6 @@ addGuessedFieldToCountryData = (dataArray) => {
     return (
       <div id="content">
         {page}
-
-        {/* <button
-          id="startBtn"
-          className={`btn ${this.state.gameStarted ? "hidden" : ""}`}
-          onClick={this.startGame}
-          type="button"
-        >
-          Start
-        </button>
-        <p id="country">Press start to begin</p>
-        <input
-          className="game-input"
-          onKeyUp={this.check}
-          id="name"
-          type="text"
-          placeholder="City..."
-        />
-        <p id="skipInfo">-</p>
-        <button onClick={this.check} id="checkAnswer" className="btn">
-          Check Answer
-        </button>
-        <button onClick={this.skip} id="skipBtn" className="btn">
-          Skip
-        </button>
-
-        <div id="scoreElements">
-          <h2 id="scoreCount">0</h2>
-          <button id="resetBtn" className="btn" onClick={this.reset}>
-            Reset Game
-          </button>
-        </div>
-        <br></br>
-        <p className="bottomLeftText" id="placesRemaining">
-          197
-        </p>
-        <p className="bottomLeftTextTwo">Countries remaining.</p>
-
-        <div id="progressBar"></div> */}
       </div>
     );
   }
--- src/components/UIElements.js
+++ src/components/UIElements.js
@@ -3,19 +3,37 @@ import React from "react";
 class UIElements extends React.Component {
   render() {
     return (
-      <>
+      <div id="background">
+
+<div class="home-section" id="home">
+        <div class="home-content">
+        
+        <ul class="circles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        </ul>
+
+        </div>
+        </div>
+
+
         <h2 id="mainTitle">Geo Quiz.</h2>
         <div id="largeBlockRight">
-          <h1 id="bgTitle">
-            GEO<br></br>QUIZ
-          </h1>
           <div className="circle small"></div>
           <div id="smallLeftBlock">
             <p id="smallLeftBlockText"></p>
           </div>
           <div id="smallRightBlock"></div>
         </div>
-      </>
+      </div>
     );
   }
 }
